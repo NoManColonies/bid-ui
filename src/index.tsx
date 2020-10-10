b12ec1b46d97cc2e6a5d0d8c889adf9996e07a52
Katsunari Miyajima <katsunari0908@gmail.com>
2020-10-10 16:36:49 +0700

    Made auth context available throughout application.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import AuthContext from "./contexts/AuthContext";
+import { Router } from "react-router";
+import { createBrowserHistory } from "history";
+import * as serviceWorker from "./serviceWorker";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContext>
+      <Router history={history}>
+        <App />
+      </Router>
+    </AuthContext>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
