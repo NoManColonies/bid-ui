7100e160e16de53d6ed9a4ecdf17471def8bb6b7
walax31 <walax31009@gmail.com>
2020-10-13 22:58:12 +0700

    add flie LoginRegister

---
+++ src/components/LoginRegister/index.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+import LoginForm from '../LoginForm'
+import RegisterForm from '../RegistrationForm'
+
+function LoginRegister (): ReactElement{
+  return(
+    <>
+    <LoginForm/>
+    <RegisterForm/>
+    </>
+  )
+}
+export default LoginRegister
