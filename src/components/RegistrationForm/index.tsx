3cda6f6624cbe7c06394621626c89631782086a3
walax31 <walax31009@gmail.com>
2020-10-14 00:23:49 +0700

    add css register page

--- src/components/RegistrationForm/index.tsx
+++ src/components/RegistrationForm/index.tsx
@@ -5,9 +5,121 @@ import { useToken } from '../../contexts/AuthContext'
 import { useRegistration } from '../../contexts/RegisterContext'
 import withHelmet from '../../utils/withHelmet'
 
-const Button = styled.button``
+const Container=styled.div`
+  position: relative;
+  width: 100%;
+  background-color: #fff;
+  min-height: 100vh;
+  overflow: hidden;
+
+  &:before{
+    content: "";
+  position: absolute;
+  height: 2000px;
+  width: 2000px;
+  top: -20%;
+  right: 48%;
+  transform: translateY(-10%);
+  background-color:#0278AE;
+  transition: 1.8s ease-in-out;
+  border-radius:30%;
+  z-index: 6;
+
+  }
+  `
+const FormsContainer=styled.div`
+ position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  `
+
+const SigninSignup=styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  left: 75%;
+  width: 50%;
+  transition: 1s 0.7s ease-in-out;
+  display: grid;
+  grid-template-columns: 1fr;
+  z-index: 5;
+  `
+
+const Signup=styled.div`
+
+`
+
+const PanelsContainer=styled.div`
+ position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+
+`
+const Panel=styled.div`
+ display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-around;
+  text-align: center;
+  z-index: 6;
+
+  `
+const RightPanel=styled.div`
+ pointer-events: none;
+  padding: 3rem 12% 2rem 17%;
+
+
+
+
+`
+
+const Form = styled.form`
+ display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0rem 5rem;
+  transition: all 0.2s 0.7s;
+  overflow: hidden;
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
+  z-index: 2;
+  `
+
+const Button = styled.button`
+  width: 150px;
+  background-color: #0278AE;
+  border: none;
+  outline: none;
+  height: 49px;
+  border-radius: 49px;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 600;
+  margin-top:2rem;
+  cursor: pointer;
+  transition: 0.5s;
+  &:hover {
+    background-color: #E8FFC1;
+    color: #0278AE;
+  }
+
+  `
 const Paragraph = styled.p``
-const Form = styled.form``
+
+const Title=styled.div`
+font-size: 2.2rem;
+  color: #444;
+  margin-bottom: 10px;
+  font-style:thicker;
+  padding-bottom:2rem;
+`
 
 function RegistrationForm(): ReactElement {
   const [
@@ -46,12 +158,18 @@ function RegistrationForm(): ReactElement {
   )
 
   return (
-    <>
+       <Container>
+       <FormsContainer>
+         <SigninSignup>
+
       {/* TODO: Style these loading indicator properly */}
       {loading && <Paragraph>...is loading</Paragraph>}
       {/* FIXME: Remove these temporary debugging component */}
       {credential.token && <Paragraph>{credential.token}</Paragraph>}
+
+      <Signup>
       <Form onSubmit={onSubmitRegister}>
+      <Title>SIGN IN</Title>
         <InputField
           type="text"
           name="username"
@@ -84,9 +202,27 @@ function RegistrationForm(): ReactElement {
             handleChangeKey(event.target.value)
           }
         />
-        <Button type="submit">Register</Button>
+        <Button type="submit">SING UP</Button>
       </Form>
-    </>
+      </Signup>
+      </SigninSignup>
+      </FormsContainer>
+
+      <PanelsContainer>
+        <Panel>
+        <RightPanel>
+        <h3>ONE OF US ?</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+          laboriosam ad deleniti.
+        </p>
+        <Button type="submit">SING IN</Button>
+
+        </RightPanel>
+        </Panel>
+      </PanelsContainer>
+      </Container>
+
   )
 }
 
