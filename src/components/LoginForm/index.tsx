d309064fce4cc9304cbaa11bf3ee6d4cf70f761b
walax31 <walax31009@gmail.com>
2020-10-14 00:23:26 +0700

    add css login page

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -4,10 +4,122 @@ import InputField from '../InputField'
 import { useToken } from '../../contexts/AuthContext'
 import { useRegistration } from '../../contexts/RegisterContext'
 import withHelmet from '../../utils/withHelmet'
+import RegistrationForm from '../RegistrationForm'
+// import Images from '../assets/1png'
 
-const Button = styled.button``
+
+
+const Container=styled.div`
+  position: relative;
+  width: 100%;
+  background-color: #fff;
+  min-height: 100vh;
+  overflow: hidden;
+
+  &:before{
+    content: "";
+  position: absolute;
+  height: 2000px;
+  width: 2000px;
+  top: -20%;
+  right: 48%;
+  transform: translateY(-10%);
+  background-color:#0278AE;
+  transition: 1.8s ease-in-out;
+  border-radius:30%;
+  z-index: 6;
+
+  }
+  `
+const FormsContainer=styled.div`
+ position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  `
+
+const SigninSignup=styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  left: 75%;
+  width: 50%;
+  transition: 1s 0.7s ease-in-out;
+  display: grid;
+  grid-template-columns: 1fr;
+  z-index: 5;
+  `
+
+const Signin=styled.div`
+  z-index: 2;
+`
+const Title=styled.div`
+font-size: 2.2rem;
+  color: #444;
+  margin-bottom: 10px;
+  font-style:thicker;
+  padding-bottom:2rem;
+`
+const PanelsContainer=styled.div`
+ position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+
+`
+const Panel=styled.div`
+ display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-around;
+  text-align: center;
+  z-index: 6;
+
+  `
+const LeftPanel=styled.div`
+pointer-events: all;
+  padding: 3rem 17% 2rem 12%;
+
+`
+
+const Form = styled.form`
+ display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0rem 5rem;
+  transition: all 0.2s 0.7s;
+  overflow: hidden;
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
+  z-index: 2;
+  `
+
+const Button = styled.button`
+  width: 150px;
+  background-color: #0278AE;
+  border: none;
+  outline: none;
+  height: 49px;
+  border-radius: 49px;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 600;
+  margin-top:2rem;
+  cursor: pointer;
+  transition: 0.5s;
+  &:hover {
+    background-color: #E8FFC1;
+    color: #0278AE;
+  }
+
+  `
 const Paragraph = styled.p``
-const Form = styled.form``
+
 
 function LoginForm(): ReactElement {
   const [
@@ -42,18 +154,22 @@ function LoginForm(): ReactElement {
   )
 
   return (
-    <>
+    <Container>
+      <FormsContainer>
+        <SigninSignup>
       {/* TODO: Style these loading indicator properly */}
       {loading && <Paragraph>...is loading</Paragraph>}
       {/* FIXME: Remove these temporary debugging component */}
       {credential.token && <Paragraph>{credential.token}</Paragraph>}
+      <Signin>
       <Form onSubmit={onSubmitLogin}>
+        <Title>WELLCOME</Title>
         <InputField
           type="text"
           name="username"
           value={username}
           onChange={(event: ChangeEvent<HTMLInputElement>): void =>
-            handleChangeUsername(event.target.value)
+          handleChangeUsername(event.target.value)
           }
         />
         <InputField
@@ -64,9 +180,28 @@ function LoginForm(): ReactElement {
             handleChangePassword(event.target.value)
           }
         />
-        <Button type="submit">Login</Button>
-      </Form>
-    </>
+        <Button type="submit">LOGIN</Button>
+        </Form>
+      </Signin>
+      </SigninSignup>
+      </FormsContainer>
+
+      <PanelsContainer>
+        <Panel>
+        <LeftPanel>
+        <h3>NEW HERE ?</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+          ex ratione. Aliquid!
+        </p>
+        <Button type="submit">SING UP</Button>
+
+        </LeftPanel>
+        </Panel>
+      </PanelsContainer>
+
+    </Container>
+
   )
 }
 
