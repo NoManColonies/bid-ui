405eb45fde1607026763b9784d95ebc963443dfa
walax31 <walax31009@gmail.com>
2020-10-13 22:57:15 +0700

    add css

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -2,7 +2,16 @@ import React, { ReactElement } from 'react'
 import InputFieldType from './InputField'
 import Styled from 'styled-components'
 
-const Input = Styled.input``
+const Input = Styled.input`
+   width: 80%;
+  outline: none;
+  border: none;
+  font-size: 1rem;
+  padding: 0.5rem 0s;
+  border-bottom: 1px solid #aaa;
+  margin-bottom:1rem;
+
+`
 
 function InputField({
   type,
