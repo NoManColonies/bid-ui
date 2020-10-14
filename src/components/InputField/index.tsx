b790f2e20addff41f091f5d9a2716092a7579252
walax31 <walax31009@gmail.com>
2020-10-14 23:50:42 +0700

    add css and placehoder

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -2,29 +2,51 @@ import React, { ReactElement } from 'react'
 import InputFieldType from './InputField'
 import Styled from 'styled-components'
 
+const InputStyle= Styled.div`
+  max-width: 380px;
+  width: 100%;
+  background-color: #f0f0f0;
+  margin: 10px 0;
+  height: 55px;
+  border-radius: 55px;
+  display: grid;
+  grid-template-columns: 15% 85%;
+  padding: 0 0.4rem;
+  position: relative;
+`
 const Input = Styled.input`
-   width: 80%;
+max-width: 380px;
+  width:200vh;
+  background: none;
   outline: none;
   border: none;
-  font-size: 1rem;
-  padding: 0.5rem 0s;
-  border-bottom: 1px solid #aaa;
-  margin-bottom:1rem;
-
+  line-height: 1;
+  font-weight: 600;
+  font-size: 1.1rem;
+  color: #333;
+  &&::-webkit-input-placeholder{
+    color: #aaa;
+    font-weight:500;
+  }
 `
+
 
 function InputField({
   type,
   name,
   value,
   onChange,
+  placeholder,
   required
 }: InputFieldType): ReactElement {
   return required ? (
-    <Input type={type} name={name} value={value} onChange={onChange} required />
+
+    <InputStyle> <Input type={type} name={name} value={value} onChange={onChange} required placeholder={placeholder}/></InputStyle>
   ) : (
-    <Input type={type} name={name} value={value} onChange={onChange} />
+    <InputStyle><Input type={type} name={name} value={value} onChange={onChange} placeholder={placeholder} /></InputStyle>
   )
+
+
 }
 
 export default InputField
