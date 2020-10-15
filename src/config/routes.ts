17a4e687a9917bf15c137c0546699ec844701d55
walax31 <walax31009@gmail.com>
2020-10-15 21:42:39 +0700

    add routes all page

---
+++ src/config/routes.ts
@@ -0,0 +1,28 @@
+import { lazy } from 'react'
+
+export default {
+  Home: {
+    path: '/',
+    exact: true,
+    component: lazy(() => import('../pages/Home'))
+  },
+
+  Bid: {
+    path: '/',
+    exact: true,
+    component: lazy(() => import('../pages/Bid'))
+  },
+
+  Payment: {
+    path: '/',
+    exact: true,
+    component: lazy(() => import('../pages/Home'))
+  },
+
+  Product: {
+    path: '/',
+    exact: true,
+    component: lazy(() => import('../pages/Product'))
+  }
+
+}
