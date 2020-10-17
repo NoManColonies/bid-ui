5254f5939a5934d910b1683d7b87f112adabea24
walax31 <walax31009@gmail.com>
2020-10-17 23:37:45 +0700

    edit css

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,40 +1,40 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-   margin: 0;
-   padding: 0;
-   width: 100vw;
-   height: 40vh;
-   background-color: #51adcf;
-   color: aliceblue;
-   display: flex;flex-direction: column;
-   align-items: center;
-   font-family: 'Josefin Sans', sans-serif;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 40vh;
+  background-color: #51adcf;
+  color: aliceblue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: 'Josefin Sans', sans-serif;
 `
 export const Content = styled.div`
-   display: flex;
-   justify-content: space-between;
-   align-items: center;
-   width: 100%;
-   height: 30vh;
-   font-family: 'Josefin Sans', sans-serif;
-
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 30vh;
+  font-family: 'Josefin Sans', sans-serif;
 `
 export const Title = styled.div`
-   display: flex;
-   padding-left: 4rem;
-   color: white;
-   font-family: 'Josefin Sans', sans-serif;
-   flex: 1;
-   width: 100%;
-   height: 30vh;
+  display: flex;
+  padding-left: 4rem;
+  color: white;
+  font-family: 'Josefin Sans', sans-serif;
+  flex: 1;
+  width: 100%;
+  height: 30vh;
 `
 
 export const Box = styled.div`
-   width: 25vw;
-   height: 30vh;
-   font-family: 'Josefin Sans', sans-serif;
-   padding-right:5rem;
+  width: 25vw;
+  height: 30vh;
+  font-family: 'Josefin Sans', sans-serif;
+  padding-right: 5rem;
 
   h3 {
     margin: 2rem 0 1rem 0;
@@ -51,13 +51,13 @@ export const Box = styled.div`
   }
 `
 export const Menu = styled.div`
-   width: 10vw;
-   height: 30vh;
+  width: 10vw;
+  height: 30vh;
 
-   display: flex;
-   flex-direction: column;
-   padding-right:3rem;
-   font-family: 'Josefin Sans', sans-serif;
+  display: flex;
+  flex-direction: column;
+  padding-right: 3rem;
+  font-family: 'Josefin Sans', sans-serif;
 
   h3 {
     margin: 2rem 0 1rem 0;
@@ -71,7 +71,6 @@ export const ActionContainer = styled.div`
   flex: 1;
   flex-direction: column;
   font-size: 0.7rem;
-
 `
 export const Action = styled.div`
   margin-bottom: 0.5rem;
@@ -83,7 +82,7 @@ export const Address = styled.div`
   height: 30vh;
   display: flex;
   flex-direction: column;
-  padding-right:4rem;
+  padding-right: 4rem;
 
   h3 {
     margin: 2rem 0 1rem 0;
@@ -100,14 +99,14 @@ export const Address = styled.div`
   }
 `
 export const Contact = styled.div`
-   margin-bottom: 0.5rem;
-   font-size: 1rem;
-   width: 15vw;
-   height: 30vh;
-   display: flex;
-   flex-direction: column;
-   padding-right:2rem;
-   font-family: 'Josefin Sans', sans-serif;
+  margin-bottom: 0.5rem;
+  font-size: 1rem;
+  width: 15vw;
+  height: 30vh;
+  display: flex;
+  flex-direction: column;
+  padding-right: 2rem;
+  font-family: 'Josefin Sans', sans-serif;
   h3 {
     margin: 2rem 0 1rem 0;
     font-weight: 600;
@@ -115,38 +114,37 @@ export const Contact = styled.div`
     font-family: 'Josefin Sans', sans-serif;
   }
   p {
-
     line-height: 1.6;
     font-size: 0.8rem;
     font-family: 'Josefin Sans', sans-serif;
-    padding-left:0.5rem
-   }
+    padding-left: 0.5rem;
+  }
 `
 
 export const User1 = styled.div`
-   display: flex;
-   flex-direction: row;
-   margin-bottom:0.5rem;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 0.5rem;
 `
 export const User2 = styled.div`
-   display: flex;
-   flex-direction: row;
-   margin-bottom:0.5rem;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 0.5rem;
 `
 export const Email = styled.div`
-   display: flex;
-   flex-direction: row;
-   margin-bottom:0.5rem;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 0.5rem;
 `
 export const Tel = styled.div`
-   display: flex;
-   flex-direction: row;
-   margin-bottom:0.5rem;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 0.5rem;
 `
 
 export const Social = styled.div`
- i{
-   width:50px;
-   height:50px;
- }
+  i {
+    width: 50px;
+    height: 50px;
+  }
 `
