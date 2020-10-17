8f069ed8964c9fa60315a7d39c7e952293c0d5bc
walax31 <walax31009@gmail.com>
2020-10-17 23:33:31 +0700

    add navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,8 +1,29 @@
 import React, { ReactElement } from 'react'
+import { Container, Wrapper, Logo, ActionContainer, Action } from '../NavBar/styles'
+import { Link } from 'react-router-dom'
 
 function NavBar(): ReactElement {
   return(
-<div></div>
+    <Container>
+    <Wrapper>
+      <Logo />
+      Logo
+      <ActionContainer>
+              <Action>
+                <Link to="/">Home</Link>
+              </Action>
+              <Action>
+                <Link to="/bid">Bid</Link>
+              </Action>
+              <Action>
+                <Link to="/product">Product</Link>
+              </Action>
+              <Action>
+                <Link to="/payment">Payment</Link>
+              </Action>
+            </ActionContainer>
+    </Wrapper>
+  </Container>
   )
 }
 export default NavBar
