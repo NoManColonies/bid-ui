768756fdb5f8bb48b037477c93f9db115ba0ef19
walax31 <walax31009@gmail.com>
2020-10-18 02:01:16 +0700

    add css

--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -1,31 +1,64 @@
 import styled from 'styled-components'
 
-interface Container{
-
-}
 
 export const Container = styled.div`
+  margin:0;
+  padding:0;
   width: 100vw;
-  position:fixed;
+  height:10vh;
   z-index: 10000;
-  background-color: ${props => (props.isScroll ? '#fff' : 'transparent')};
-  color:${props => (props.isScroll ? '#000' : '#fff')};
+  background-color:#0278AE;
+  color:white;
   transition:background-color 0.5;
 `
 
-export const Logo = styled.div``
+export const Box = styled.div`
+  display: flex;
+  flex-direction:row;
+  justify-content: space-between;
+  align-items: center;
+  padding-top:1.5rem;
+  margin-left:2rem;
+
+
+`
+
+export const Logo = styled.div`
+
+align-items: center;
+`
+export const Join = styled.div`
+ margin-right:2rem;
+ align-items: center;
+ display: flex;
+ width:10vw;
+ flex-direction:row;
+ justify-content:center;
+
+
+`
 
 export const Wrapper = styled.div`
   display: flex;
-  flex: 1;
-  padding: 2rem;
+  flex-direction:column;
+  justify-content:center;
+
+
 `
 
 export const ActionContainer = styled.ul`
+
   display: flex;
-  flex: 1;
-  justify-content: flex-end;
+  flex-direction:row;
+  justify-content:center;
+  align-items:center;
 `
 export const Action = styled.li`
   padding: 0 1rem;
+  font-size:0.8rem;
+
+  &:hover{
+    color:#E8FFC1;
+    font-size:0.9rem;
+  }
 `
