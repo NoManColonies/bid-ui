701a132806e802d65b8daeb3f3a3cd8b1ff4123d
walax31 <walax31009@gmail.com>
2020-10-18 13:50:01 +0700

    edit function

--- src/components/BackgroundBlock.tsx
+++ src/components/BackgroundBlock.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 interface BackgroundBlockType {
    src: string;
    height?: string;
-   children: ReactElement[] | ReactElement;
+
 }
 interface BackgroundBlockPropsType{
   src: string;
@@ -19,25 +19,14 @@ const BackgroundImage = styled.div`
   background-size: cover;
   background-position: center;
   background-attachment: fixed;
-
   position: relative;
 `
-const BackgroundOverylay = styled.div`
-  position: absolute;
-  width: 100vw;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.4);
 
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`
-function BackgroundBlock ({ src, height = "100", children }: BackgroundBlockType): ReactElement {
+function BackgroundBlock ({ src, height = "100" }: BackgroundBlockType): ReactElement {
   return (
     <div>
       <BackgroundImage src={src} height={height}>
-        <BackgroundOverylay>{children}</BackgroundOverylay>
+
       </BackgroundImage>
     </div>
   )
