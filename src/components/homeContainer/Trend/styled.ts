cf697155439be0b328e821924d9cab39e1b8af9b
walax31 <walax31009@gmail.com>
2020-10-24 02:54:21 +0700

    add css to styled file

---
+++ src/components/homeContainer/Trend/styled.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+export const Container =styled.div`
+width:100vw;
+height:100vh;
+background-color:red;
+display:flex;
+
+`
+export const BoxXL = styled.div`
+ width:100vw;
+ height:100%;
+ background-color:#e61d1d;
+ display:flex;
+ justify-content:center;
+ align-items:center;
+ margin-left:1rem;
+`
+export const BoxL = styled.div`
+ width:80vw;
+ height:100%;
+ background-color:#8af869;
+ display:flex;
+ flex-direction:row;
+
+`
+
+export const BoxM = styled.div`
+ width:40vw;
+ height:100%;
+ background-color:#69f8ec;
+ display:flex;
+ flex-direction:column;
+`
+export const BoxS = styled.div`
+ width:40vw;
+ height:50%;
+ background-color:green;
+ display:flex;
+ flex-direction:row;
+
+`
+export const BoxSx = styled.div`
+ width:20vw;
+ height:100%;
+ background-color:#a9b8a9;
+
+`
+
