7b911f4067263a0da5219b6530deed750af7a8b3
walax31 <walax31009@gmail.com>
2020-10-24 02:55:37 +0700

    move css to styled file

--- src/components/NavBar/SearchBar/index.tsx
+++ src/components/NavBar/SearchBar/index.tsx
@@ -1,45 +1,5 @@
 import React, { useState,ReactElement,FormEvent,ChangeEvent } from "react";
-import styled from 'styled-components'
-
-
-const Container =styled.div`
-    padding-top:1rem;
-    display:flex;
-    width:34vw;
-    justify-content:flex-end;
-
-`
-
-
-const Form =styled.form`
-
-      width: 200px;
-      height: 40px;
-      border-radius:10px 10px 10px 10px;
-      border:none;
-      font-family: 'Josefin Sans', sans-serif;
-
-
-
-
-`
-
-const SearchInput=styled.input`
-      width: 200px;
-      height: 25px;
-     border-radius:10px 10px 10px 10px;
-     border:none;
-     background: white;
-     outline: none;
-     padding-left:0.5rem;
-     font-family: 'Josefin Sans', sans-serif;
-
-     &::placeholder{
-       padding-left:0.5rem;
-       font-family: 'Josefin Sans', sans-serif;
-     }
-
-`
+import {Container,Form,SearchInput} from './styled'
 
 
 
