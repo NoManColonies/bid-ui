8e96f0186c64d5852e46a36aef11ebe2b7921584
walax31 <walax31009@gmail.com>
2020-10-24 22:44:38 +0700

    edit css

--- src/components/homeContainer/HotProduct/styled.ts
+++ src/components/homeContainer/HotProduct/styled.ts
@@ -2,56 +2,55 @@ import styled from 'styled-components'
 
 export const Container =styled.div`
 width:100vw;
-height:100vh;
+height:155vh;
 background-color:#f7f7f7;
+padding-left:3rem;
+padding-top:3rem;
 
 h1{
-  font-weight:800;
-  font-size:1.2rem;
+  font-size:2rem;
+  word-spacing:1px;
   text-transform: uppercase;
-  margin-left:8rem;
-  padding-top:3.5rem;
-
+  font-family: 'Staatliches', cursive;
  }
 
 `
 
 export const ProductBox =styled.div`
  width:100vw;
- height:90vh;
+ height:100vh;
  display:flex;
-
+ margin:0;
+ padding:0;
  justify-content:center;
- margin-top:1rem;
 
 `
 
 export const CulumnBox =styled.div`
  width:100vw;
- height:90vh;
  display:flex;
+ margin:0;
+ padding:0;
  flex-direction:column;
- justify-content:center;
+
 
 
 `
 export const RowBox =styled.div`
  width:100vw;
- height:40vh;
  padding:0;
  display:flex;
  flex-direction:row;
- justify-content:center;
  margin-top:0;
 
 `
 
 export const Product =styled.div`
- width:12vw;
- height:30vh;
+ width:29.5vw;
+ height:70vh;
  background-color:white;
- margin-left:1.9rem;
- margin-right:1.9rem;
+ margin-right:1rem;
+ margin-top:2rem;
 
 
  &:hover{
