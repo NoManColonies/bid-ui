90461b336bea46d1f88b80b4be0f9dd0c5b84cd2
walax31 <walax31009@gmail.com>
2020-10-24 16:50:29 +0700

    edit import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react'
 import withHelmet from '../utils/withHelmet'
-import backgroundImage from '../assets/nike_0.jpg'
+import backgroundImage from '../assets/backgraund/nike_0.jpg'
 import BackgroundBlock from '../components/BackgroundBlock'
 import Container from '../components/homeContainer'
 import Footer from '../components/Footer'
