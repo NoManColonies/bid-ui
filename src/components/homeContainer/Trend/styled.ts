9ea8db7286d20e98c7c90dc67e71bddae2d01580
walax31 <walax31009@gmail.com>
2020-10-24 16:50:13 +0700

    edit css

--- src/components/homeContainer/Trend/styled.ts
+++ src/components/homeContainer/Trend/styled.ts
@@ -2,48 +2,35 @@ import styled from 'styled-components'
 
 export const Container =styled.div`
 width:100vw;
-height:100vh;
-background-color:red;
-display:flex;
+height:135vh;
+
+ background-color:#f7f7f7;
+ h1{
+  font-weight:800;
+  font-size:1.2rem;
+  text-transform: uppercase;
+  margin-left:8rem;
+  padding-top:3rem;
+  padding-bottom:3rem;
+
+
+ }
 
 `
 export const BoxXL = styled.div`
  width:100vw;
- height:100%;
- background-color:#e61d1d;
+ height:75%;
  display:flex;
  justify-content:center;
  align-items:center;
- margin-left:1rem;
+
 `
 export const BoxL = styled.div`
  width:80vw;
  height:100%;
- background-color:#8af869;
- display:flex;
- flex-direction:row;
-
-`
-
-export const BoxM = styled.div`
- width:40vw;
- height:100%;
- background-color:#69f8ec;
- display:flex;
- flex-direction:column;
-`
-export const BoxS = styled.div`
- width:40vw;
- height:50%;
- background-color:green;
- display:flex;
- flex-direction:row;
-
-`
-export const BoxSx = styled.div`
- width:20vw;
- height:100%;
- background-color:#a9b8a9;
+ display:grid;
+ grid-template-columns:repeat(4,25%);
+ grid-template-rows:50% 50%;
 
 `
 
