eb84b3a24e19d6eeca142660dabe90bbb6b89c0a
walax31 <walax31009@gmail.com>
2020-10-25 22:33:30 +0700

    css styles component profile user

---
+++ src/components/Profile/ProfileUser/styled.ts
@@ -0,0 +1,196 @@
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+export const Container =styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction:column;
+
+width:100vw;
+height:100vh;
+ h1{
+  font-size:2rem;
+  word-spacing:1px;
+  text-transform: uppercase;
+  font-family: 'Staatliches', cursive;
+  margin-bottom:1rem;
+  margin-top:-4rem;
+ }
+`
+
+export const UserWrapper =styled.div`
+width:70vw;
+height:75vh;
+background-color:white;
+display:flex;
+flex-direction:row;
+
+box-shadow: 2px 2px 4px gray;
+
+`
+export const UserFormWrapper =styled.div`
+width:50%;
+height:75vh;
+background-color:#51adcf;
+position:relative;
+overflow:hidden;
+display:flex;
+justify-content:center;
+align-items:center;
+`
+
+
+export const Image =styled.div`
+width:150px;
+height:150px;
+border-radius:50%;
+background-color:#dad8d8;
+margin-bottom:1.5rem;
+`
+
+export const Card =styled.div`
+width:50%;
+height:75vh;
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction:column;
+
+
+`
+export const Form =styled.form`
+width:100%;
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction:column;
+flex-wrap:wrap;
+position:absolute;
+transform:translateX(${(props: {click: boolean}): string=> props.click?'-100%' : '0'});
+transition:all 500ms ease-in;
+`
+export const Form2 =styled.form`
+width:100%;
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction:column;
+flex-wrap:wrap;
+position:absolute;
+transform:translateX(100%);
+transform:translateX(${(props: {click: boolean}): string=> props.click?'0%' : '100%'});
+transition:all 500ms ease-in;
+`
+
+export const Button =styled.button`
+  width: 150px;
+  background-color: #0278ae;
+  border:none;
+  outline: none;
+  height: 49px;
+  border-radius: 49px;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 600;
+  margin-top: 1rem;
+  cursor: pointer;
+  transition: 0.5s;
+
+  &:hover {
+    background-color: #e8ffc1;
+    color: #0278ae;
+    border: none;
+  }
+`
+
+export const IdCardWrapper =styled.div`
+ width:100%;
+ height:50%;
+ display:flex;
+ justify-content:center;
+ align-items:center;
+ flex-direction:column;
+
+
+`
+
+
+export const IdImageWrapper =styled.div`
+ width:70%;
+ height:80%;
+ background-color:#eaeaf3;
+ display:flex;
+ justify-content:center;
+ align-items:center;
+ text-align:center;
+ border-radius:10px;
+
+`
+export const IdImage =styled.div`
+ width:70%;
+ height:80%;
+ img{
+  width:90%;
+  height:80%;
+
+  }
+  h2{
+    padding-top:1rem;
+  }
+
+`
+export const Status =styled.div`
+width:100%;
+height:30%;
+
+h2{
+  font-size:0.8rem;
+  word-spacing:1px;
+  text-transform: uppercase;
+   margin-left:4rem;
+  margin-bottom:0.5rem;
+}
+`
+export const Icon =styled(FontAwesomeIcon)`
+    display: block;
+    color:#51adcf;
+    padding-left:1rem;
+`
+export const IconWrapper =styled.div`
+  display: flex;
+  top: auto;
+  color: #fff;
+  flex: 1;
+  color:#51adcf;
+  /* text-align: center; */
+  `
+  export  const TitleWrapper =styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+`
+
+export const Title =styled.div`
+  font-size: 0.7rem;
+  font-weight:800;
+  text-transform: uppercase;
+  padding-left:1rem;
+  bottom:0;
+
+
+`
+export const Value =styled.div`
+  font-size: 1rem;
+  font-weight: bold;
+  line-height: 1rem;
+  padding-left:1rem;
+`
+export const FormStatus =styled.div`
+
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`
